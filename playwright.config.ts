6cdae1749075c6c576d425f6e894ccb072565c15
Szabolcs Hudak <szabolcshudak@Szabolcss-Laptop.local>
2026-09-10 16:44:55 +0200

    ci: split API and browser test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,17 +41,33 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: "api",
+      testMatch: /tests\/api\/.*\.spec\.ts/,
+    },
+    {
       name: "chromium",
+      testMatch: [
+        /tests\/ui\/.*\.spec\.ts/,
+        /tests\/smoke\/.*\.spec\.ts/,
+      ],
       use: { ...devices["Desktop Chrome"] },
     },
 
     {
       name: "firefox",
+      testMatch: [
+        /tests\/ui\/.*\.spec\.ts/,
+        /tests\/smoke\/.*\.spec\.ts/,
+      ],
       use: { ...devices["Desktop Firefox"] },
     },
 
     {
       name: "webkit",
+      testMatch: [
+        /tests\/ui\/.*\.spec\.ts/,
+        /tests\/smoke\/.*\.spec\.ts/,
+      ],
       use: { ...devices["Desktop Safari"] },
     },
 
